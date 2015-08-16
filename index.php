73a1fa0b9a14c5e63de95ef02c2dabbf170d728c
unknown <shazreenamohdshaipul@gmail.com>
2015-08-16 11:26:03 +0800

    text

--- index.php
+++ index.php
@@ -3,3 +3,9 @@
 echo "Hello git UTM";
 
 echo "intro to git basic";
+
+echo "Text messaging, or texting, is the act of composing and sending brief, electronic messages between two or more mobile phones, or fixed or portable devices over a phone network. The term originally referred to messages sent using the Short Message Service (SMS). It has grown to include messages containing image, video, and sound content (known as MMS messages). The sender of a text message is known as a texter, while the service itself has different colloquialisms depending on the region. It may simply be referred to as a text in North America, the United Kingdom, Australia, New Zealand and the Philippines, an SMS in most of mainland Europe, and an MMS or SMS in the Middle East, Africa, and Asia.
+
+Text messages can be used to interact with automated systems to, for example, to order products or services, or to participate in contests. Advertisers and service providers use direct text marketing to message mobile phone users about promotions, payment due dates, et cetera instead of using mail, e-mail or voicemail.
+
+In a straight and concise definition for the purposes of this English language article, text messaging by phones or mobile phones should include all 26 letters of the alphabet and 10 numerals, i.e., alpha-numeric messages, or text, to be sent by texter or received by the textee."
